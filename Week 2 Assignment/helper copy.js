bd4912d338ee954555ac79c56f45e6b53200aee7
OniiKuma <105169315+OniiKuma@users.noreply.github.com>
2023-01-18 13:03:53 -0700

    Add files via upload

---
+++ Week 2 Assignment/helper copy.js
@@ -0,0 +1,11 @@
+function displayText() {
+    document.getElementById("welcomeToPage").innerHTML = "Welcome to my Information Page";
+}
+
+function displayText2(myId , displayText) {
+    document.getElementById(myId).innerHTML = displayText;
+}
+
+function displayText3(myId , displayText) {
+    document.getElementById(myId).innerHTML = displayText;
+}
---
+++ Week 2 Assignment/helper.js
@@ -0,0 +1,11 @@
+function displayText() {
+    document.getElementById("aboutMe").innerHTML = "About Me";
+}
+
+function displayText2(myId , displayText) {
+    document.getElementById(myId).innerHTML = displayText;
+}
+
+function displayText3(myId , displayText) {
+    document.getElementById(myId).innerHTML = displayText;
+}
